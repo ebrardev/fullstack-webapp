12d908570473c49d542e537bcdf23eb9b0e12cd9
ebrardev <16155052@mersin.edu.tr>
2024-05-11 14:42:26 +0300

    add prisma client

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,22 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const  register = async (req,res) =>{
     const {username,email,password} = req.body;
 
     //? hast the password
 
-    const hashedPassword = await bcrypt.hashSync(password,10);
+    const hashedPassword = await bcrypt.hash(password,10);
    console.log(hashedPassword)
     //? create a new user
+
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password:hashedPassword
+        }
+    })
 }
 
 export const  login = (req,res) => {
