39289103c93853945844ee22f308ab4ca7acabe2
ebrardev <16155052@mersin.edu.tr>
2024-05-13 17:47:27 +0300

    cors

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"
 
 function Login() {
+  const [error,setError] = useState("")
+  const navigate= useNavigate()
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+    const formData = new FormData(e.target)
+
+    const username= formData.get("username")
+    const email= formData.get("email")
+    const password= formData.get("password")
+
+   try{
+    const res = await  axios.post("http://localhost:8080/api/auth/register",{
+      username,email,password
+
+    })
+   navigate("/login")
+   }catch(err){
+
+  console.log(err)
+  setError(err.response.data.message)
+
+   }
+  }
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit} >
           <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="password" type="password" placeholder="Password" />
+          <input name="username" required minLength={3} maxLength={20} type="text" placeholder="Username" />
+          <input name="password" required type="password" placeholder="Password" />
           <button>Login</button>
+          {error && <span> {error}!</span>}
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
