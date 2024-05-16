89999b8c54ad15f267d7e43cd2ecd5f9a2619707
ebrardev <16155052@mersin.edu.tr>
2024-05-16 23:45:57 +0300

    delete

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -57,8 +57,15 @@ export const updateUser = async (req,res) =>{
     }
 }
 export const deleteUser = async (req,res) =>{
-    try{
 
+    const id = req.params.id;
+    const tokenUserId = req.userId;
+
+    try{
+   await prisma.user.delete({
+    where:{id}
+   })
+   res.status(200).json({message:"user deleted"})
     }catch(err) {
         console.log(err)
         res.status(500).json({message:"failed to get users"})
