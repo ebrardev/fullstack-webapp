651878d672c63a283a0c5639ac4bffe04cbf9c58
ebrardev <16155052@mersin.edu.tr>
2024-05-17 23:50:44 +0300

    post model

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -3,8 +3,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get("/test",(req,res)=>{
-    res.send("Hello World!");
-})
+router.get("/", getPosts)
+router.get("/:id",getPost)
+router.post("/",addPost)
+router.put("/:id",updatePost)
+router.delete("/:id",deletePost)
 
 export default router;
