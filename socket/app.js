b8ef55662bb2ebe25fdcea718fa3833c6fdb123d
ebrardev <16155052@mersin.edu.tr>
2024-05-20 15:34:14 +0300

    socket io

---
+++ socket/app.js
@@ -0,0 +1,39 @@
+import { on } from "nodemon";
+import { Server } from "socket.io";
+
+const io = new Server({
+    cors:{
+        origin:"http://localhost:5173",
+    }
+})
+
+let onlineUser = []
+
+const addUser = (userId,socketId) =>{
+    const userExits= onlineUser.find(user =>user.userId === userId)
+
+    if(userExits) {
+        onlineUser.push({userId,socketId})
+    }
+}
+
+const removeUser = (socketId)=>{
+    onlineUser = onlineUser.filter(user=>user.socketId !== socketId)
+}
+
+const getUser = (userId) =>{
+    return onlineUser.find((user) =>user.userId === userId)
+}
+
+
+io.on("connection",(socket)=>{
+  socket.on("newUser",(userId)=>{
+    addUser(userId,socket.id)
+  })
+  socket.on("disconnect",()=>{
+    removeUser(socket.id)
+
+  })
+})
+
+io.listen("4000")
