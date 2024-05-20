e9c0a28c2084e27f8ac5bf10c05e6ff986a03fbd
ebrardev <16155052@mersin.edu.tr>
2024-05-20 12:01:21 +0300

    getChats

--- api/controllers/chat.controller.js
+++ api/controllers/chat.controller.js
@@ -2,12 +2,53 @@ import prisma from "../lib/prisma.js";
 
 
 export const getChats = async (req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    const tokenUserId = req.userId
+
+    try {
+        const chats = await prisma.chat.findMany({
+            where:{
+                userIDs:{
+                    hasSome:[tokenUserId]
+                }
+            }
+        })
+
+
+    res.status(200).json(chats);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get chats!" });
   }
 };
 
+
+export const getChat = async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+      res.status(200).json(users);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to get chat!" });
+    }
+  };
+  
+  export const addChat = async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+      res.status(200).json(users);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to get addChats!" });
+    }
+  };
+  
+  export const readChat = async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+      res.status(200).json(users);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to get read chat!" });
+    }
+  };
+  
