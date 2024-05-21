e6ac3c35f63d86f7f7f40739ea211471774b6305
ebrardev <16155052@mersin.edu.tr>
2024-05-21 12:40:47 +0300

    read

--- socket/app.js
+++ socket/app.js
@@ -1,43 +1,41 @@
-import { on } from "nodemon";
 import { Server } from "socket.io";
 
 const io = new Server({
-    cors:{
-        origin:"http://localhost:5173",
-    }
-})
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
-let onlineUser = []
+let onlineUser = [];
 
-const addUser = (userId,socketId) =>{
-    const userExits= onlineUser.find(user =>user.userId === userId)
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
 
-    if(userExits) {
-        onlineUser.push({userId,socketId})
-    }
-}
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
 
-const removeUser = (socketId)=>{
-    onlineUser = onlineUser.filter(user=>user.socketId !== socketId)
-}
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
 
-const getUser = (userId) =>{
-    return onlineUser.find((user) =>user.userId === userId)
-}
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
 
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
 
-io.on("connection",(socket)=>{
-  socket.on("newUser",(userId)=>{
-    addUser(userId,socket.id)
-  })
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
 
-  socket.on("sendMessage",({receiverId,data})=>{
-    console.log(receiverId)
-  })
-  socket.on("disconnect",()=>{
-    removeUser(socket.id)
-
-  })
-})
-
-io.listen("4000")
+io.listen("4000");
